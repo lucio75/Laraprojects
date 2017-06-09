a83b5a43ff7048d86f29cdfa5226db1fcb7ed69c
lucio75 <lucioticali@gmail.com>
2017-06-09 21:03:09 +0200

    'change'

--- resources/views/images/albumimages.blade.php
+++ resources/views/images/albumimages.blade.php
@@ -17,6 +17,10 @@
             <td>
                 <img width="120" src="{{asset($image->img_path)}}"/>
             </td>
+            <td>
+                <a href="{{route('photos.destroy',$image->id)}}"
+                   class="btn btn-danger">DELETE</a>
+            </td>
         </tr>
         @empty
             <tr><td colspan="5">
@@ -25,3 +29,35 @@
             @endforelse
 </table>
     @endsection
+
+@section('footer')
+    @parent
+    <script>
+        $('document').ready(function(){
+            //$('div.alert').fadeOut(3000);
+            $('table').on('click','a.btn-danger',function(e){
+                e.preventDefault();
+                var urlAlbum=$(this).attr('href');
+                var tr = e.target.parentNode.parentNode;
+                $.ajax(urlAlbum,
+                        {
+                            method: 'DELETE',
+                            data :{
+                                '_token' : '{{csrf_token()}}'
+                            },
+                            complete : function(resp){
+                                console.log(resp);
+                                if(resp.responseText == 1){
+                                    //alert(resp.responseText);
+                                    tr.parentNode.removeChild(tr);
+
+                                }else{
+                                    alert('Problem connecting server');
+                                }
+                            }
+                        }
+                )
+            });
+        });
+    </script>
+@endsection
